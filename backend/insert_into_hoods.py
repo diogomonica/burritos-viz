83c31ddaa4fb828cd1dc6733ddc01186819666c2
F$ git config --global user.email your_email@youremail.comSets the email of the user for all git instances on the system <diogo.monica@gmail.com>
2011-06-26 11:48:36 -0700

    Added script to insert hoods from uber_hood.csv to the sqlite database uber.db.

---
+++ backend/insert_into_hoods.py
@@ -0,0 +1,11 @@
+import sqlite3
+conn = sqlite3.connect('uber.db')
+f = open('uber_hood.csv', 'r')
+for line in f.readlines():
+  args = line.split(",")
+  print args
+  trip_id= args[0]
+  origin = args[1]
+  destination = args[2]
+  conn.execute('''insert into hoods values("%s","%s","%s")''' % (trip_id,origin,destination))
+conn.commit()
